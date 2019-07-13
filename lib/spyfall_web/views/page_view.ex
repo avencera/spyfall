ac4856243ea33af7619962d4a87989bf27fe0931
Praveen Perera <ppraveen25@gmail.com>
2019-07-13 16:48:30 -0400

    Edit button styling

--- lib/spyfall_web/views/page_view.ex
+++ lib/spyfall_web/views/page_view.ex
@@ -1,3 +1,7 @@
 defmodule SpyfallWeb.PageView do
   use SpyfallWeb, :view
+
+  def index_button_class(true, _), do: "gray-button-outline"
+  def index_button_class(false, "new"), do: "indigo-button mr-1"
+  def index_button_class(false, "edit"), do: "indigo-button-outline"
 end
