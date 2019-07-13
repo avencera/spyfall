112b6785b22f38e688b0844ead4b173a3aca2d51
Praveen Perera <ppraveen25@gmail.com>
2019-07-13 13:14:50 -0400

    Rename game state to status

--- lib/spyfall/game.ex
+++ lib/spyfall/game.ex
@@ -2,9 +2,9 @@ defmodule Spyfall.Game do
   alias Spyfall.Game
   alias Spyfall.Game.{Player, Registry}
 
-  # state = :waiting | :in_progress | :ended
-  @enforce_keys [:id, :state]
-  defstruct [:id, players: [], state: :waiting]
+  # status = :waiting | :in_progress | :ended
+  @enforce_keys [:id, :players, :status]
+  defstruct [:id, players: [], status: :waiting]
 
   def get(%Game{} = game), do: get(game.id)
   def get(game_id), do: Registry.get_game(game_id)
@@ -13,7 +13,7 @@ defmodule Spyfall.Game do
     game_id = generate_game_id()
 
     with :not_found <- get(game_id),
-         game <- %Game{id: game_id, state: :waiting, players: [player]},
+         game <- %Game{id: game_id, status: :waiting, players: [player]},
          :ok <- Registry.register_or_replace_game(game) do
       {:ok, game}
     else
@@ -23,17 +23,18 @@ defmodule Spyfall.Game do
   end
 
   def start(%Game{} = game) do
-    {:ok, %{game | state: :in_progress}}
+    {:ok, %{game | status: :in_progress}}
   end
 
   def finish(%Game{} = game) do
-    {:ok, %{game | state: :ended}}
+    {:ok, %{game | status: :ended}}
   end
 
   def delete(%Game{} = game), do: Registry.delete_game(game)
 
   def join(game_id, player_name) do
     with {:ok, game} <- get(game_id),
+         :waiting <- game.status,
          %Player{} = player <- Player.create(player_name),
          {:ok, game, player} <- add_player(game, player),
          :ok <- Registry.register_or_replace_game(game) do
@@ -56,17 +57,7 @@ defmodule Spyfall.Game do
     {:ok, %{game | players: players}}
   end
 
-  def can_player_enter_game_room?(game, player) do
-    with {:ok, game} <- get(game.id),
-         true <- game_has_player?(game, player) do
-      true
-    else
-      _ ->
-        false
-    end
-  end
-
-  defp game_has_player?(game, player) do
+  def game_has_player?(game, player) do
     player_ids =
       game
       |> Map.get(:players)
