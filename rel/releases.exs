d7765aeba08a0c76a5b0f8db3b5e26602dbf5b76
Praveen Perera <ppraveen25@gmail.com>
2019-07-17 15:29:30 -0400

    Add libcluster, add release configuration

---
+++ rel/releases.exs
@@ -0,0 +1,23 @@
+import Config
+
+config :logger, level: String.to_atom(System.get_env("LOGGER_LEVER", "info"))
+
+config :spyfall, Spyfall.Repo,
+  url: System.fetch_env!("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE", "10"))
+
+config :spyfall, SpyfallWeb.Endpoint,
+  http: [:inet6, port: String.to_integer(System.get_env("PORT", "4000"))],
+  secret_key_base: System.fetch_env!("SECRET_KEY_BASE")
+
+config :libcluster,
+  topologies: [
+    spyfall: [
+      strategy: Elixir.Cluster.Strategy.Kubernetes.DNS,
+      config: [
+        service: System.get_env("SERVICE_NAME", "spyfall"),
+        application_name: "spyfall",
+        polling_interval: 5_000
+      ]
+    ]
+  ]
